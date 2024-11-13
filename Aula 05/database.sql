8aee33d192a5f9ef0a1f83ef74f46c1ec40410e1
filipeoliveira-03 <luiz.fmoc0311@gmail.com>
2024-11-13 08:59:28 -0400

    Aula 05 finalizada

---
+++ Aula 05/database.sql
@@ -0,0 +1,6 @@
+INSERT INTO pessoas VALUES
+(DEFAULT, 'Ana', '1975-12-22', 'f', '52.3', '1.45', 'EUA'),
+(DEFAULT, 'Pedro', '2000-07-15', 'm', '52.3', '1.45', DEFAULT),
+(DEFAULT, 'Maria', '1999-05-30', 'f', '75.9', '1.70', 'Portugal');
+
+SELECT * FROM pessoas;
