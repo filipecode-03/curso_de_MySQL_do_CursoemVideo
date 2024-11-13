f6ba661b0e06b78a0d1d87812bcf698cd7b8c6c7
filipeoliveira-03 <luiz.fmoc0311@gmail.com>
2024-11-13 08:33:36 -0400

    Aula 03 dinalizada

---
+++ Aula 03/database.sql
@@ -0,0 +1,11 @@
+create database cadastro;
+CREATE TABLE pessoas (
+    nome VARCHAR(30),
+    idade TINYINT(3),
+    sexo CHAR(1),
+    peso FLOAT,
+    altura FLOAT,
+    nacionalidade VARCHAR(20)
+);
+
+DESCRIBE pessoas; 
